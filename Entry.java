168c63cf841cda659f2eb48ab234a7aaf8ce4aa9
Rohit Agarwal <agarwal.rohit0910@gmail.com>
2020-05-06 14:03:50 +0530

    Clean up for uploading

--- Entry.java
+++
@@ -1,9 +0,0 @@
-public class Entry<K extends Comparable<K>, T> {
-    K key;
-    T entity;
-
-    Entry(K k, T t) {
-        key = k;
-        entity = t;
-    }
-}
--- Node.java
+++
@@ -1,9 +0,0 @@
-public class Node<K extends Comparable<K>, T> {
-    Node<K, T> l = null;
-    Node<K, T> r = null;
-    Entry<K, T> value;
-
-    Node(Entry<K, T> value) {
-        this.value = value;
-    }
-}
